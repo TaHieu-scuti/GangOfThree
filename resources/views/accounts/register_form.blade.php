ef55f05a3d3c3571d47977b9fd0e34b4458ce727
Truong Pham <truongpxc@gmail.com>
2018-07-28 12:25:26 +0700

    Add register form

--- resources/views/accounts/register_form.blade.php
+++ resources/views/accounts/register_form.blade.php
@@ -35,59 +35,32 @@
     <label>Tên đầy đủ</label>
     <div class="two fields">
       <div class="field">
-        <input type="text" name="name" placeholder="First Name">
+        <input type="text" name="fullname" placeholder="">
       </div>
     </div>
   </div>
   <div class="fields">
     <div class="seven wide field">
       <label>Ngày sinh</label>
-      <input type="text" name="card[number]" maxlength="16" placeholder="DD/MM/YYYY">
+      <input type="text" name="dob">
     </div>
   </div>
-   <h4 class="ui dividing header">Receipt</h4>
-   <div class="field">
-    <label>Send Receipt To:</label>
-    <div class="ui fluid multiple search selection dropdown">
-      <input type="hidden" name="receipt">
-      <i class="dropdown icon"></i>
-      <div class="default text">Saved Contacts</div>
-      <div class="menu">
-        <div class="item" data-value="jenny" data-text="Jenny">
-          <img class="ui mini avatar image" src="/images/avatar/small/jenny.jpg">
-          Jenny Hess
-        </div>
-        <div class="item" data-value="elliot" data-text="Elliot">
-          <img class="ui mini avatar image" src="/images/avatar/small/elliot.jpg">
-          Elliot Fu
-        </div>
-        <div class="item" data-value="stevie" data-text="Stevie">
-          <img class="ui mini avatar image" src="/images/avatar/small/stevie.jpg">
-          Stevie Feliciano
-        </div>
-        <div class="item" data-value="christian" data-text="Christian">
-          <img class="ui mini avatar image" src="/images/avatar/small/christian.jpg">
-          Christian
-        </div>
-        <div class="item" data-value="matt" data-text="Matt">
-          <img class="ui mini avatar image" src="/images/avatar/small/matt.jpg">
-          Matt
-        </div>
-        <div class="item" data-value="justen" data-text="Justen">
-          <img class="ui mini avatar image" src="/images/avatar/small/justen.jpg">
-          Justen Kitsune
-        </div>
-      </div>
+  <div class="fields">
+    <div class="seven wide field">
+      <label>Ảnh đại diện</label>
+      <input type="file" name="avatar">
     </div>
   </div>
-   <div class="ui segment">
-    <div class="field">
-      <div class="ui toggle checkbox">
-        <input type="checkbox" name="gift" tabindex="0" class="hidden">
-        <label>Do not include a receipt in the package</label>
-      </div>
+  <div class="fields">
+    <div class="seven wide field">
+      <label>Facebook</label>
+      <input type="text" name="facebook">
+    </div>
+    <div class="seven wide field">
+      <label>Skype</label>
+      <input type="text" name="skype">
     </div>
   </div>
-  <div class="ui button" tabindex="0">Submit Order</div>
+  <div class="ui button" tabindex="0">Đăng ký</div>
 </form>
 @endsection
