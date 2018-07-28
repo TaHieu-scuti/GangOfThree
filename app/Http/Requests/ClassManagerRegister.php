304f5f475665ef66140af734a9ae20e1deb63095
Duong Nguyen <duongnx@scuti.asia>
2018-07-28 12:42:48 +0700

    added UserRegister and ClassManagerRegister

---
+++ app/Http/Requests/ClassManagerRegister.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClassManagerRegister extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'avatar' => 'max:10000|mimes:jpeg,png,jpg',
+            'fullname' => 'required|string|max:100',
+            'facebook' => 'string|max:255',
+            'skype' => 'string|max:255',
+            'degree' => 'string|max:100',
+            'description' => 'string',
+        ];
+    }
+}
